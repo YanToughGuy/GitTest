c4bc215272fdb0a49e482267c27ff8147b784791
yan <2890091705@qq.com>
2022-07-29 18:06:34 +0800

    master2

--- src/main/java/com/bdqn/Test.java
+++ src/main/java/com/bdqn/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is a Git Test");
         System.out.println("master git1");
+        System.out.println("master git2");
     }
 }
