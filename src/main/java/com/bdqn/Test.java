822939d3784a12494c927bc9a730926156765508
yan <2890091705@qq.com>
2022-07-29 18:06:49 +0800

    master3

--- src/main/java/com/bdqn/Test.java
+++ src/main/java/com/bdqn/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("This is a Git Test");
         System.out.println("master git1");
         System.out.println("master git2");
+        System.out.println("master git3");
     }
 }
