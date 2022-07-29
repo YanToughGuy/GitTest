ac73ddab95ee8496661dc7a55926137a65602bd7
yan <2890091705@qq.com>
2022-07-29 18:06:08 +0800

    master1

---
+++ src/main/java/com/bdqn/Test.java
@@ -0,0 +1,8 @@
+package com.bdqn;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is a Git Test");
+        System.out.println("master git1");
+    }
+}
